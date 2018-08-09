163d3b5dda437d7b9bc177299740307a7f0e0c9f
HN <nhai.qn@gmail.com>
2018-08-08 23:18:44 -0400

    update notebok + related scripts

--- moledit/amber_builder.py
+++ moledit/amber_builder.py
@@ -11,27 +11,27 @@ class AmberBuilder(Leapify):
     '''
 
     def build_protein(self, *args, **kwargs):
-        from pdb4amber.builder.pytraj_build import build_protein
+        from .builder.pytraj_build import build_protein
         self.parm = build_protein(*args, **kwargs)
         return self
 
     def build_bdna(self, *args, **kwargs):
-        from pdb4amber.builder.pytraj_build import build_bdna
+        from .builder.pytraj_build import build_bdna
         self.parm = build_bdna(*args, **kwargs)
         return self
 
     def build_adna(self, *args, **kwargs):
-        from pdb4amber.builder.pytraj_build import build_adna
+        from .builder.pytraj_build import build_adna
         self.parm = build_adna(*args, **kwargs)
         return self
 
     def build_arna(self, *args, **kwargs):
-        from pdb4amber.builder.pytraj_build import build_arna
+        from .builder.pytraj_build import build_arna
         self.parm = build_arna(*args, **kwargs)
         return self
 
     def solvate(self, *args, **kwargs):
-        from pdb4amber.builder.pytraj_build import solvate
+        from .builder.pytraj_build import solvate
         self.parm = solvate(self.parm, *args, **kwargs)
         return self
 
--- moledit/builder/builder.py
+++ moledit/builder/builder.py
@@ -1,7 +1,7 @@
 import warnings
 import parmed as pmd
 
-from ..compat import StringIO
+from io import StringIO
 
 warnings.filterwarnings("ignore")
 
